080e5c469ff98f58d2e0d9e32eddba6d37290bb6
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-02 13:53:23 +0300

    Create Main.vue

---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<script>
+
+    export default {
+
+        name: 'Main',
+
+        props: {
+            title: String
+        }
+
+    }
+
+</script>
+
+<template>
+
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
