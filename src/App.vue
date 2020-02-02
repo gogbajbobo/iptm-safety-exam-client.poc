09056d91a9ffdf5e661e65cb9f94fda2c25145fe
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-02 13:53:33 +0300

    use Main.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,40 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/iptm_logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  },
-  created() {
-    this.$socket.emit('message', 'App.vue created')
-  }
-}
+    import Main from './components/Main'
+
+    export default {
+        name: 'app',
+        components: {
+            Main
+        },
+        created() {
+            this.$socket.emit('message', 'App.vue created')
+        }
+    }
+
 </script>
 
+<template>
+
+    <div id="app">
+
+        <img alt="Vue logo" src="./assets/iptm_logo.png">
+        <Main title="Экзамены по охране труда и технике безопасности"/>
+
+    </div>
+
+</template>
+
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
 </style>
