217b7906e399d344c0905c93ed161129f8bf66d4
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-04 16:33:42 +0300

    added login route

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/Login'
+
 
 Vue.use(Router)
 
+const login = {
+    path: '/login',
+    name: Login.name,
+    component: Login,
+    meta: {
+        anonymousAccess: true
+    }
+}
+
+const routes = [
+    login
+]
+
 const router = new Router({
+    routes
+})
+
+const anonymousAccessRoutes = routes
+    .filter(route => route.meta && route.meta.anonymousAccess)
+    .map(route => route.name)
+
+router.beforeEach((to, from, next) => {
+
+    const isAuthorized = false
+
+    if (!isAuthorized) {
+
+        if (anonymousAccessRoutes.includes(to.name)) {
+            return next()
+        } else {
+            return next({ name: Login.name })
+        }
+
+    }
+
+    return next()
+
 })
 
 export default router
