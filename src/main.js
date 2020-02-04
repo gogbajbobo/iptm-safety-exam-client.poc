c724ac905a32e56431fb8a00a079a379babe0989
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-04 16:15:47 +0300

    Vue.use(Router)

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueSocketIO from 'vue-socket.io'
 
 const isProduction = process.env.NODE_ENV === 'production'
+import router from '@/router'
 
 Vue.config.productionTip = false
 
@@ -12,5 +13,6 @@ Vue.use(new VueSocketIO({
 }))
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
