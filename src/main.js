ad5071816737d33fe4697a6f6570b3a5d17c9f02
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-04 15:41:27 +0300

    debug if not production

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueSocketIO from 'vue-socket.io'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 Vue.config.productionTip = false
 
 Vue.use(new VueSocketIO({
-  debug: true,
+  debug: !isProduction,
   connection: 'http://localhost:8081'
 }))
 
