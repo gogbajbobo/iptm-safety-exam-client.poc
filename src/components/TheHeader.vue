c57e0dd27f2b72cc8550156ce1c8d00c580455fd
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-04 16:32:59 +0300

    Create TheHeader.vue

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<script>
+
+    export default {
+
+        name: 'TheHeader'
+
+    }
+
+</script>
+
+<template>
+
+    <div>
+
+        <img alt="Vue logo" src="../assets/iptm_logo.png">
+        <h1>Экзамены по охране труда и технике безопасности</h1>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
