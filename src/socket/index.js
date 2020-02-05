6f179138c78b04624bf1a3fae6a73ec1dedda630
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-05 14:11:03 +0300

    intermediate commit

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
 import events from '@/socket/events'
+import store, { SOCKET_ACTION_PREFIX } from '@/store'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 const socket = new VueSocketIO({
     debug: !isProduction,
-    connection: 'http://localhost:8081'
+    connection: 'http://localhost:8081',
+    vuex: {
+        store,
+        actionPrefix: SOCKET_ACTION_PREFIX
+    }
 })
 
 Vue.use(socket)
--- src/socket/io.js
+++ src/socket/io.js
@@ -2,6 +2,7 @@ import socket from '@/socket'
 import { SOCKET } from '@/socket/events'
 import logger from '@/services/logger'
 import router from '@/router'
+// import store, { SOCKET_ACTION_PREFIX } from '@/store'
 
 const { io } = socket
 
@@ -11,6 +12,9 @@ const login = data => {
 
         if (response.error)
             return logger.error('auth error')
+
+        // store.dispatch(`${ SOCKET_ACTION_PREFIX }${ SOCKET.LOGIN }`, 'test')
+        //     .catch(logger.error)
 
         router.push({ name: 'Main' })
             .catch(logger.error)
