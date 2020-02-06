0f973ff13590ea2a36aefc202dbb78c3efdae50c
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-06 14:57:58 +0300

    openSocket, closeSocket

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,8 +1,24 @@
 import io from 'socket.io-client'
+import logger from '@/services/logger'
 
 const isProduction = process.env.NODE_ENV === 'production'
 isProduction || (localStorage.debug = 'socket.io*')
 
-const socket = io('http://localhost:8081')
+const socketMock = { emit: () => logger.warn('socket not created') }
+let socket = socketMock
+
+export const openSocket = ({ username, password }) => {
+
+    socket = io('http://localhost:8081', { autoConnect: false, query: { username, password } })
+    return socket
+
+}
+
+export const closeSocket = () => {
+
+    socket.close()
+    socket = socketMock
+
+}
 
 export default socket
