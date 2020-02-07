17590b87b983c858ce218621d1c809f5f0d2813b
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-07 14:38:12 +0300

    Create requests.js

---
+++ src/services/requests.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import logger from '@/services/logger';
+
+
+const axiosInstance = axios.create()
+
+const
+    authPath = `/auth`,
+    loginUrl = `${ authPath }/login`
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+axiosInstance.defaults.baseURL =
+    isProduction
+        ? 'http://localhost:8081'
+        : 'http://localhost:8081'
+
+
+export const login = ({ username, password }) => {
+
+    const data = { username, password }
+
+    return axiosInstance.post(loginUrl, data)
+        .then(response => logger.info(response))
+        .catch(logger.error)
+
+}
