03966dbb9093576e95659d821655050cf413e820
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-13 15:22:06 +0300

    comment out unused

--- src/socket/io.js
+++ src/socket/io.js
@@ -1,23 +1,23 @@
-import { openSocket, closeSocket } from '@/socket'
-import { SOCKET } from '@/socket/events'
-import logger from '@/services/logger'
-import store from '@/store'
+// import { openSocket, closeSocket } from '@/socket'
+// import { SOCKET } from '@/socket/events'
+// import logger from '@/services/logger'
+// import store from '@/store'
+//
+// const login = loginData => {
+//
+//     const socket = openSocket(loginData)
+//
+//     socket.on(SOCKET.AUTHORIZED, user => {
+//
+//         logger.info('socket authorize:', user)
+//         user || closeSocket()
+//
+//         store.dispatch(`login`, user)
+//             .catch(logger.error)
+//
+//     })
+//     socket.connect()
+//
+// }
 
-const login = loginData => {
-
-    const socket = openSocket(loginData)
-
-    socket.on(SOCKET.AUTHORIZED, user => {
-
-        logger.info('socket authorize:', user)
-        user || closeSocket()
-
-        store.dispatch(`login`, user)
-            .catch(logger.error)
-
-    })
-    socket.connect()
-
-}
-
-export default { login }
+export default {  }
