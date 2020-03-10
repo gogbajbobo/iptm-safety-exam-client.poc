76f29810c3306e34a3048bb846269486fa06204b
gogbajbobo <grigoriev.maxim@gmail.com>
2020-03-10 15:03:27 +0300

    show admin menu

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,9 +2,25 @@
 
     import socket from '@/socket'
 
+    import helper from '@/services/helper'
+
+    import store from '@/store'
+    import { getters } from '@/store/constants'
+
+    import AdminMenu from '@/components/AdminMenu'
+
     export default {
 
         name: 'Main',
+
+        components: { AdminMenu },
+
+        computed: {
+
+            user() { return store.getters[getters.user] },
+            isAdmin() { return helper.isAdmin(this.user) },
+
+        },
 
         methods: {
             testButtonClicked() { socket.sendMessageToServer('test button clicked') }
@@ -17,7 +33,13 @@
 <template>
 
     <div>
+
         <button @click="testButtonClicked">Test</button>
+
+        <div v-if="isAdmin">
+            <admin-menu></admin-menu>
+        </div>
+
     </div>
 
 </template>
