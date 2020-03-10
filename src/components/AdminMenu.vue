9b04cc3088fd0b7fade889a2a8d3872befd92ad6
gogbajbobo <grigoriev.maxim@gmail.com>
2020-03-10 15:03:17 +0300

    Create AdminMenu.vue

---
+++ src/components/AdminMenu.vue
@@ -0,0 +1,31 @@
+<script>
+
+    import logger from '@/services/logger'
+
+    export default {
+
+        name: 'AdminMenu',
+
+        methods: {
+
+            examButtonPressed() { logger.info('exam button pressed') }
+
+        }
+
+    }
+
+</script>
+
+<template>
+
+    <div>
+
+        <button @click="examButtonPressed">Экзамены</button>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
