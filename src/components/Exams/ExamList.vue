83c17315c7de27bff3970201a2bc30a281441e95
gogbajbobo <grigoriev.maxim@gmail.com>
2020-03-11 16:41:23 +0300

    show list of exams

--- src/components/Exams/ExamList.vue
+++ src/components/Exams/ExamList.vue
@@ -3,9 +3,28 @@
     import router from '@/router'
     import { paths } from '@/router/paths'
 
+    import store from '@/store'
+    import { actions, getters } from '@/store/constants'
+
+    import { messages } from '@/services/messages'
+
     export default {
 
         name: 'ExamList',
+
+        computed: {
+            exams() { return store.getters[getters.exams] }
+        },
+
+        mounted() {
+
+            const loader = this.$loading.show()
+
+            return store.dispatch(actions.getExams)
+                .catch(messages.error)
+                .then(loader.hide)
+
+        },
 
         methods: {
             addExamButtonPressed() { router.push(paths.EXAM_FORM) }
@@ -19,7 +38,17 @@
 
     <div>
 
-        Exam List
+        <h2>Список экзаменов:</h2>
+
+        <template v-for="exam in exams">
+
+            <article :key="exam.id">
+
+                <h3>{{ exam.id }}. {{ exam.title }}</h3>
+
+            </article>
+
+        </template>
 
         <button @click="addExamButtonPressed">Добавить экзамен</button>
 
