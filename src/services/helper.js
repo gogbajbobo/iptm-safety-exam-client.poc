38ef7bad00c379e9b8bd141b84561ae2240fd8b9
gogbajbobo <grigoriev.maxim@gmail.com>
2020-03-12 14:08:06 +0300

    helper.loaderWithAction

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,5 +1,14 @@
+import {messages} from '@/services/messages'
+
 const isProduction = process.env.NODE_ENV === 'production'
 
 const isAdmin = user => user.roles.includes('admin')
 
 export default { isProduction, isAdmin }
+const loaderWithAction = (caller, action) => {
+
+    const loader = caller.$loading.show()
+    return action.catch(messages.error).then(loader.hide)
+
+}
+
