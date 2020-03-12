a25bd7cbdf99657a66dc55cdf0bc0992decac86e
gogbajbobo <grigoriev.maxim@gmail.com>
2020-03-12 13:29:18 +0300

    added confirm method

--- src/services/messages.js
+++ src/services/messages.js
@@ -1,4 +1,5 @@
 const alert = text => window.alert(text)
 const error = text => window.alert(`Ошибка: ${ text }`)
+const confirm = text => window.confirm(text)
 
-export const messages = { alert, error }
+export const messages = { alert, error, confirm }
