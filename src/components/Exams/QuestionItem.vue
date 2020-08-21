cb11fe2fc340e92afa64d895b2bc4ceb0512a1f6
gogbajbobo <grigoriev.maxim@gmail.com>
2020-08-21 13:45:44 +0300

    Create QuestionItem.vue

---
+++ src/components/Exams/QuestionItem.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+
+    name: 'QuestionItem',
+
+    props: {
+        question: { type: Object },
+    },
+
+}
+
+</script>
+
+<template>
+
+<div>
+
+    {{ question }}
+    <hr>
+
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
