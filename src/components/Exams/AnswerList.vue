84eaeb837b588f5abcd76ced120904dbffaaa3ad
gogbajbobo <grigoriev.maxim@gmail.com>
2020-08-23 15:05:34 +0300

    getAnswers()

--- src/components/Exams/AnswerList.vue
+++ src/components/Exams/AnswerList.vue
@@ -1,8 +1,35 @@
 <script>
 
+import store from '@/store'
+import { actions, getters } from '@/store/constants'
+import { helper } from '@/services/helper'
+
 export default {
 
     name: 'AnswerList',
+
+    props: {
+        questionId: { type: [ String, Number ] },
+    },
+
+    computed: {
+        answers() { return store.getters[getters.answers] },
+    },
+
+    mounted() {
+        this.getAnswers()
+    },
+
+    methods: {
+
+        getAnswers() {
+
+            const action = store.dispatch(actions.getAnswers, { question: this.questionId })
+            return helper.loaderWithAction(this, action)
+
+        },
+
+    },
 
 }
 
