62395a496e09e20fd36fe84bfeb099321eaa1efc
gogbajbobo <grigoriev.maxim@gmail.com>
2020-08-23 15:25:53 +0300

    add exams and questions buttons

--- src/components/Exams/AnswerForm.vue
+++ src/components/Exams/AnswerForm.vue
@@ -1,8 +1,36 @@
 <script>
 
+import router from '@/router'
+import { paths } from '@/router/paths'
+
+import store from '@/store'
+import { getters } from '@/store/constants'
+
 export default {
 
-    name: 'AnswerForm'
+    name: 'AnswerForm',
+
+    props: {
+        examId: { type: [ String, Number ] },
+        questionId: { type: [ String, Number ] },
+        answerId: { type: [ String, Number ] },
+    },
+
+    computed: {
+
+        exam() { return store.getters[getters.exams].find(exam => exam.id === Number(this.examId)) },
+        question() { return store.getters[getters.questions].find(q => q.id === Number(this.questionId)) },
+        answer() { return store.getters[getters.answers].find(a => a.id === Number(this.answerId)) },
+        answerTitle() { return this.answer ? this.answer.text : 'Новый ответ' },
+
+    },
+
+    methods: {
+
+        examsButtonPressed() { router.push({ path: paths.EXAM_LIST }) },
+        questionsButtonPressed() { router.push({ path: `${ paths.EXAM_FORM }/${ this.examId }` }) },
+
+    },
 
 }
 
@@ -11,7 +39,17 @@ export default {
 <template>
 
     <div>
-        AnswerForm
+
+        <button @click="examsButtonPressed">Экзамены</button>
+
+        <fieldset>
+
+            <legend>Экзамен: {{ exam.title }}</legend>
+
+            <button @click="questionsButtonPressed">Вопросы</button>
+
+        </fieldset>
+
     </div>
 
 </template>
