d143e436921a82ff9d439195ebd4652a3a051e15
gogbajbobo <grigoriev.maxim@gmail.com>
2020-09-02 12:40:34 +0300

    Create ExamineeMenu.vue

---
+++ src/components/ExamineeMenu.vue
@@ -0,0 +1,41 @@
+<script>
+
+    import router from '@/router'
+    import { paths } from '@/router/paths'
+
+    import { createTestExam } from '@/services/testExamGenerator'
+
+    export default {
+
+        name: 'ExamineeMenu',
+
+        methods: {
+
+            examButtonPressed() { router.push(paths.EXAM_LIST) },
+
+            testExamCreateButtonPressed() { createTestExam() },
+
+        }
+
+    }
+
+</script>
+
+<template>
+
+    <div class="examinee-menu">
+
+        <button @click="examButtonPressed">Список экзаменов</button>
+        <button @click="testExamCreateButtonPressed">Создать тестовый экзамен</button>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+    .admin-menu button {
+        margin: 10px;
+    }
+
+</style>
