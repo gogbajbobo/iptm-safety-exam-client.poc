215c1d59675d77a5c389d434f3ebb4c83c86e32b
gogbajbobo <grigoriev.maxim@gmail.com>
2020-09-03 15:41:12 +0300

    Create ExamTakingQuestion.vue

---
+++ src/components/Exams/ExamTakingQuestion.vue
@@ -0,0 +1,33 @@
+<script>
+
+    import ExamTakingAnswer from '@/components/Exams/ExamTakingAnswer'
+
+
+    export default {
+
+        name: 'ExamTakingQuestion',
+
+        components: { ExamTakingAnswer },
+
+        props: {
+            question: { type: Object },
+        },
+
+    }
+
+</script>
+
+<template>
+
+    <div>
+        {{ question.id }} {{ question.text }}
+        <template v-for="answer in question.answers">
+            <exam-taking-answer :answer="answer" :key="answer.id"></exam-taking-answer>
+        </template>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
