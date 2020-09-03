e9b8635c65f7d90e518f49ddabb14f5389b1b2ce
gogbajbobo <grigoriev.maxim@gmail.com>
2020-09-03 15:41:08 +0300

    Create ExamTakingAnswer.vue

---
+++ src/components/Exams/ExamTakingAnswer.vue
@@ -0,0 +1,25 @@
+<script>
+
+    export default {
+
+        name: 'ExamTakingAnswer',
+
+        props: {
+            answer: { type: Object },
+        },
+
+    }
+
+</script>
+
+<template>
+
+    <div>
+        {{ answer.id }} {{ answer.text }}
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
