c9f36f1d3acde135cfe20085cd4060a60e8321d8
gogbajbobo <grigoriev.maxim@gmail.com>
2020-09-03 15:42:00 +0300

    getQuestions to take exam

--- src/components/Exams/ExamTaking.vue
+++ src/components/Exams/ExamTaking.vue
@@ -1,10 +1,52 @@
 <script>
 
-export default {
+    import { helper } from '@/services/helper'
+    import { userRoles } from '@/services/constants'
 
-    name: 'ExamTaking',
+    import store from '@/store'
+    import { actions, getters } from '@/store/constants'
 
-}
+    import ExamTakingQuestion from '@/components/Exams/ExamTakingQuestion'
+
+
+    export default {
+
+        name: 'ExamTaking',
+
+        components: { ExamTakingQuestion },
+
+        props: {
+            examId: { type: String },
+        },
+
+        computed: {
+
+            user() { return store.getters[getters.user] },
+            questions() { return store.getters[getters.questions] },
+            isExaminee() { return helper.isExaminee(this.user) },
+
+        },
+
+        mounted() {
+            this.getQuestions()
+        },
+
+        methods: {
+
+            getQuestions() {
+
+                if (!this.isExaminee) return
+
+                const payload = { exam: this.examId, role: userRoles.examinee }
+
+                const action = store.dispatch(actions.getQuestions, payload)
+                return helper.loaderWithAction(this, action)
+
+            },
+
+        },
+
+    }
 
 </script>
 
@@ -12,6 +54,11 @@ export default {
 
     <div>
         ExamTaking
+
+        <template v-for="question in questions">
+            <exam-taking-question :question="question" :key="question.id"></exam-taking-question>
+        </template>
+
     </div>
 
 </template>
