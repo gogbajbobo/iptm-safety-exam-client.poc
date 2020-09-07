d4cf9896e7e80075d7ab53cc68f22d3373ad42a6
gogbajbobo <grigoriev.maxim@gmail.com>
2020-09-07 11:22:14 +0300

    getResults

--- src/components/ExamResults/ExamResultList.vue
+++ src/components/ExamResults/ExamResultList.vue
@@ -1,8 +1,37 @@
 <script>
 
+    import store from '@/store'
+    import { actions } from '@/store/constants'
+
+    import { helper } from '@/services/helper'
+
+    import ExamResultItem from '@/components/ExamResults/ExamResultItem'
+
+
     export default {
 
         name: 'ExamResultList',
+
+        components: { ExamResultItem },
+
+        data() {
+            return {
+                examResults: [],
+            }
+        },
+
+        mounted() {
+            this.getResults()
+        },
+
+        methods: {
+            getResults() {
+                return helper.loaderWithAction(
+                    this,
+                    store.dispatch(actions.getExamResults).then(results => this.examResults = results)
+                )
+            },
+        },
 
     }
 
@@ -11,6 +40,17 @@
 <template>
 
     <div>
+
+        <h2>Результаты:</h2>
+
+        <hr>
+
+        <template v-for="examResult in examResults">
+
+            <exam-result-item :examResult="examResult"
+                              :key="examResult.id"></exam-result-item>
+
+        </template>
 
     </div>
 
